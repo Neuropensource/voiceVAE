2fbd009f52037baaa95ca0095c6b0c148359872d
Hippolyte <hippolytedreyfus@orange.fr>
2023-04-24 17:00:00 +0200

    modfied slurm.sh

--- slurm.sh
+++ slurm.sh
@@ -1,13 +1,13 @@
-# !/bin/bash
-# SBATCH --partition=besteffort
-# SBATCH --nodes=1
-# SBATCH --ntasks-per-node=3
-# SBATCH --gres=gpu:1
-# SBATCH --cpus-per-task=1
-# SBATCH --job-name="spectroVAE"
-# SBATCH --time=3:00:00
-# SBATCH --output="output_screen"
-# SBATCH --error="output_error"
+#!/bin/bash
+#SBATCH --partition=besteffort
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=3
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name="spectroVAE"
+#SBATCH --time=3:00:00
+#SBATCH --output="output_screen"
+#SBATCH --error="output_error"
 # echo " $SLURM_ARRAY_TASK_ID "
 conda activate charly
 srun python trainingSimple.py 
