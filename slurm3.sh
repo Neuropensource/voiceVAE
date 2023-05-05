e3ab16eb8a738c6dae482e3a41c91c07bc5c8b6e
Hippolyte <hippolytedreyfus@orange.fr>
2023-05-05 15:47:51 +0200

    slurm3

--- slurm3.sh
+++ slurm3.sh
@@ -6,8 +6,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --job-name="vanillaVAE"
 #SBATCH --time=24:00:00
-#SBATCH --output="cluster_outputs/Out_slurm2"
-#SBATCH --error="cluster_outputs/Err_slurm2"
+#SBATCH --output="cluster_outputs/Out_slurm3"
+#SBATCH --error="cluster_outputs/Err_slurm3"
 # echo " $SLURM_ARRAY_TASK_ID "
 
 echo hello_world
